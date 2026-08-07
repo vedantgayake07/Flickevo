f6d9f8e1ba236866228afd6be9eba7ef02a639c9
vedantgayake <vedantgayake07@gmail.com>
2026-08-07 19:58:29 +0530

    filename conflict scrollToTop

--- Cinevo-client/src/layout/appLayout.jsx
+++ Cinevo-client/src/layout/appLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 import Footer from "./footer";
-import ScrollToTop from "../components/ScrollToTop";
+import ScrollToTop from "../components/scrollToTop";
 
 const AppLayout = () => {
   return (
